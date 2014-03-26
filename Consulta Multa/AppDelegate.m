21a2d688a95e6e7e11a7046c94e83d63731648fa
Ernesto Carrión <ernesto@kogimobile.com>
2014-03-26 11:39:53 -0500

    Added methods for executing underlying javascript

--- Consulta Multa/AppDelegate.m
+++ Consulta Multa/AppDelegate.m
@@ -7,13 +7,19 @@
 //
 
 #import "AppDelegate.h"
+#import "ConsultaNavigationViewController.h"
+#import "ChooseViewController.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
+    
+    ChooseViewController * cvc = [[ChooseViewController alloc] init];
+    ConsultaNavigationViewController * navController = [[ConsultaNavigationViewController alloc] initWithRootViewController:cvc];
+    self.window.rootViewController = navController;
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
